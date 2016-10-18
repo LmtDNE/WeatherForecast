739fa93d386d26b5bc2ecab9ad223e105bd2bef3
Dion <Dion.Fulwood@gmail.com>
2016-10-17 22:54:43 -0700

    Adds axios get request to fetch weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
-
-import Private from "../private"
-
-
+import axios from 'axios'
+import Private from "../../private"
 const API_KEY = Private.API_KEY;
+
+//Exported variable for type to maintain consistancy
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
